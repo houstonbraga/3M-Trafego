e3cad436a370e8d66643e68d4c7690398d25a80e
Houston Braga <houstonliverboy123@gmail.com>
2025-10-06 22:17:11 -0300

    feat: add footer and water mark

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,13 @@ import Degrade from "./assets/degrade.png";
 import Texture from "./assets/texture.png";
 import ButtonsGroup from "./components/ButtonsGroup";
 import { ArrowDownRight } from "lucide-react";
+import Marca from './assets/marca.png'
 
 const Home = () => {
   return (
     <LayoutConfig>
       <div
-        className="content-screen relative overflow-hidden w-screen sm:w-[440px] text-white bg-black min-h-[762px] rounded-xl flex flex-col px-5"
+        className="content-screen relative overflow-hidden w-screen sm:w-[440px] text-white bg-black min-h-[762px] rounded-xl flex flex-col pb-8 px-5"
         style={{
           backgroundImage: `url(${Texture})`,
           backgroundSize: "cover",
@@ -28,6 +29,14 @@ const Home = () => {
           <h2 className="text-sm">Links úteis</h2>
         </div>
         <ButtonsGroup />
+
+        {/* Footer */}
+        <footer className="pt-16 text-center">
+          <p className="text-[10px] text-gray-700">
+            3M© | Todos os direitos reservados
+          </p>
+        </footer>
+      <img src={Marca} alt="marca de agua" className="w-full absolute bottom-0 right-0 left-0"/>
       </div>
     </LayoutConfig>
   );
--- src/components/ButtonsGroup.jsx
+++ src/components/ButtonsGroup.jsx
@@ -1,15 +1,20 @@
 import { MoveRight } from "lucide-react";
+import Whats from '../assets/whats.svg'
+import Instagram from '../assets/instagram.svg'
+import Folder from '../assets/portfolio.svg'
 
 const ButtonsGroup = () => {
   return (
-    <div className="flex flex-col w-full px-3 mt-5 gap-5">
+    <div className=" z-10 flex flex-col w-full px-3 mt-5 gap-5">
       <a
         href="#"
         className="flex justify-between items-center w-full h-[100px] bg-black border border-[#823cfa] rounded-2xl group hover:scale-105 transition-transform duration-300"
       >
         <MoveRight color="#823CFA" className="ml-5 animate-slide-x" />
         <h2>WhatsApp</h2>
-        <div className="h-full w-[100px] bg-[#823cfa] rounded-br-xl rounded-tr-xl"></div>
+        <div className="h-full w-[100px] flex justify-center items-center bg-[#823cfa] rounded-br-xl rounded-tr-xl">
+          <img src={Whats} alt="whats-icon" />
+        </div>
       </a>
       <a
         href="#"
@@ -17,7 +22,19 @@ const ButtonsGroup = () => {
       >
         <MoveRight color="#823CFA" className="ml-5 animate-slide-x" />
         <h2>Instagram</h2>
-        <div className="h-full w-[100px] bg-[#823cfa] rounded-br-xl rounded-tr-xl"></div>
+        <div className="h-full w-[100px] flex justify-center items-center bg-[#823cfa] rounded-br-xl rounded-tr-xl">
+          <img src={Instagram} alt="insta-icon" />
+        </div>
+      </a>
+      <a
+        href="#"
+        className="flex justify-between items-center w-full h-[130px] bg-black border border-[#823cfa] rounded-2xl group hover:scale-105 transition-transform duration-300"
+      >
+        <MoveRight color="#823CFA" className="ml-5 animate-slide-x" />
+        <h2>Portfólio</h2>
+        <div className="h-full w-[100px] flex justify-center items-center bg-[#823cfa] rounded-br-xl rounded-tr-xl">
+          <img src={Folder} alt="folder-icon" />
+        </div>
       </a>
     </div>
   );
