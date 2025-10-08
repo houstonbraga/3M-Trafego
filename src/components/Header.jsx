ab79c60d99351d48446026d09aa06d66f5d7dd43
Houston Braga <houstonliverboy123@gmail.com>
2025-10-07 22:55:06 -0300

    test: test logo with div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,20 @@ import Verification from "../assets/verification.png";
 const Header = () => {
   return (
     <div className="flex flex-col ml-8 z-10 mt-16 gap-2">
-      <img src={Logo} alt="logo" width={100}/>
+      <div className="content-center">
+        <img src={Logo} alt="logo"/>
+      </div>
       <div className="flex gap-1 items-end">
         <div className="flex flex-col justify-start">
           <h1 className="font-bold text-xl">3M Gestão</h1>
           <h1 className="font-bold text-xl">de tráfego</h1>
         </div>
-        <img src={Verification} width={24} alt="verification-icon" className="mb-1"/>
+        <img
+          src={Verification}
+          width={24}
+          alt="verification-icon"
+          className="mb-1"
+        />
       </div>
       <p className="text-xs text-gray-500">Especialista em tráfego pago |</p>
       <p className="text-xs text-gray-500">Estrategista digital |</p>
